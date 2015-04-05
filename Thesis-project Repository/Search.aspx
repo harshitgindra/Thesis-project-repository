b46d6712f619e57a0130083b1220d8fa5c805c70
karvand <kaveharvand@gmail.com>
2015-04-05 14:41:29 -0500

    Omni search and Advanced search use cases added
    It is suggested to move AACTYPE field from logininfo  to userinfo table

---
+++ Thesis-project Repository/Search.aspx
@@ -0,0 +1,168 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="Thesis_project_Repository.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h2>Omni Search</h2>
+    
+        Enter a keyword:<br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Label ID="Label6" runat="server" ForeColor="Red"></asp:Label>
+        <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator1" ControlToValidate="TextBox1" runat="server" ErrorMessage="RequiredFieldValidator" ForeColor="Red" ></asp:RequiredFieldValidator>--%>
+        <br />
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Search" />
+        <br />
+        <br />
+       <h2>Advanced Search:</h2>
+        <asp:DropDownList ID="DropDownList1" runat="server" Height="22px" Width="166px" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" >
+            <asp:ListItem Text="Select Search Criterion" />
+            <asp:ListItem>username</asp:ListItem>
+            <asp:ListItem>first name</asp:ListItem>
+            <asp:ListItem>last name</asp:ListItem>
+            <asp:ListItem>email</asp:ListItem>
+            <asp:ListItem>phone number</asp:ListItem>
+            <asp:ListItem>account type</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+        <div id="div1"  runat="server">
+        <table>
+            <tr><td>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server"  RepeatDirection="Horizontal" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem>Search All</asp:ListItem>
+            <asp:ListItem>Search by Keyword</asp:ListItem> 
+        </asp:RadioButtonList></td>
+               <td> <asp:TextBox ID="TextBox2" runat="server" visible="False"></asp:TextBox>
+                   <asp:Label ID="Label1" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+        <%--<td><asp:RequiredFieldValidator ID="RequiredFieldValidator2" ControlToValidate="TextBox2" runat="server" ErrorMessage="RequiredFieldValidator" ForeColor="Red"></asp:RequiredFieldValidator></td>--%>
+        </tr></table> </div>
+        <div id="div2"  runat="server" visible="false">
+        <table>
+            <tr><td>
+        <asp:RadioButtonList ID="RadioButtonList2" runat="server"  RepeatDirection="Horizontal" OnSelectedIndexChanged="RadioButtonList2_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem>Search All</asp:ListItem>
+            <asp:ListItem>Search by Type</asp:ListItem> 
+        </asp:RadioButtonList></td>
+               <td><asp:DropDownList ID="DropDownList2" runat="server" AppendDataBoundItems="True" DataSourceID="TypeSqlDataSource" DataTextField="ACCTYPE" DataValueField="ACCTYPE" Visible="False"><asp:ListItem Text="Select Type" /></asp:DropDownList> 
+                   <asp:SqlDataSource ID="TypeSqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:it485projectConnectionString %>" SelectCommand="SELECT DISTINCT [ACCTYPE] FROM [LOGININFO]"></asp:SqlDataSource>
+               <asp:Label ID="Label2" runat="server" ForeColor="Red"></asp:Label>
+                    </td>
+        </tr></table> </div>
+         <div id="div3"  runat="server" visible="false">
+        <table>
+            <tr><td>
+        <asp:RadioButtonList ID="RadioButtonList3" runat="server"  RepeatDirection="Horizontal" OnSelectedIndexChanged="RadioButtonList3_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem>Search by Keyword</asp:ListItem> 
+        </asp:RadioButtonList></td>
+               <td> <asp:TextBox ID="TextBox3" runat="server" visible="False"></asp:TextBox>
+                   <asp:Label ID="Label3" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+        <%--<td><asp:RequiredFieldValidator ID="RequiredFieldValidator2" ControlToValidate="TextBox2" runat="server" ErrorMessage="RequiredFieldValidator" ForeColor="Red"></asp:RequiredFieldValidator></td>--%>
+        </tr></table> </div>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Search" />
+        <asp:Label ID="Label4" runat="server" ForeColor="Red"></asp:Label>
+        <br />
+        <br />
+        <div id="div4"  runat="server">
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="USERNAME" DataSourceID="UserInfoSqlDataSource" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+                <asp:BoundField DataField="FIRSTNAME" HeaderText="FIRSTNAME" SortExpression="FIRSTNAME" />
+                <asp:BoundField DataField="LASTNAME" HeaderText="LASTNAME" SortExpression="LASTNAME" />
+                <asp:BoundField DataField="USERNAME" HeaderText="USERNAME" ReadOnly="True" SortExpression="USERNAME" />
+                <asp:BoundField DataField="EMAILID" HeaderText="EMAILID" SortExpression="EMAILID" />
+                <asp:BoundField DataField="PHONENO" HeaderText="PHONENO" SortExpression="PHONENO" />
+            </Columns>
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+        <br />
+        <asp:DetailsView ID="DetailsView1" runat="server" AutoGenerateRows="False" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="4" DataKeyNames="USERNAME" DataSourceID="UserDetailsSqlDataSource" ForeColor="Black" GridLines="Horizontal" Height="50px" Width="125px">
+            <EditRowStyle BackColor="#CC3333" Font-Bold="True" ForeColor="White" />
+            <Fields>
+                <asp:BoundField DataField="FIRSTNAME" HeaderText="FIRSTNAME" SortExpression="FIRSTNAME" />
+                <asp:BoundField DataField="LASTNAME" HeaderText="LASTNAME" SortExpression="LASTNAME" />
+                <asp:BoundField DataField="USERNAME" HeaderText="USERNAME" ReadOnly="True" SortExpression="USERNAME" />
+                <asp:BoundField DataField="EMAILID" HeaderText="EMAILID" SortExpression="EMAILID" />
+                <asp:BoundField DataField="PHONENO" HeaderText="PHONENO" SortExpression="PHONENO" />
+            </Fields>
+            <FooterStyle BackColor="#CCCC99" ForeColor="Black" />
+            <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
+        </asp:DetailsView>
+        <asp:DetailsView ID="DetailsView2" runat="server" AutoGenerateRows="False" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="4" DataSourceID="SqlDataSource1" ForeColor="Black" GridLines="Horizontal" Height="35px" Width="169px">
+            <EditRowStyle BackColor="#CC3333" Font-Bold="True" ForeColor="White" />
+            <Fields>
+                <asp:BoundField DataField="ACCTYPE" HeaderText="ACCTYPE" SortExpression="ACCTYPE" />
+            </Fields>
+            <FooterStyle BackColor="#CCCC99" ForeColor="Black" />
+            <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
+        </asp:DetailsView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:it485projectConnectionString %>" SelectCommand="SELECT [ACCTYPE] FROM [LOGININFO] WHERE ([USERNAME] = @USERNAME)">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="GridView1" DefaultValue="0" Name="USERNAME" PropertyName="SelectedValue" Type="String" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+        <asp:SqlDataSource ID="UserDetailsSqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:it485projectConnectionString %>" SelectCommand="SELECT * FROM [USERINFO] WHERE ([USERNAME] = @USERNAME)">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="GridView1" Name="USERNAME" PropertyName="SelectedValue" Type="String" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+        <asp:SqlDataSource ID="UserInfoSqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:it485projectConnectionString %>" SelectCommand="SELECT * FROM [USERINFO] WHERE (([LASTNAME] LIKE '%' + @LASTNAME + '%') OR ([USERNAME] LIKE '%' + @USERNAME + '%') OR ([FIRSTNAME] LIKE '%' + @FIRSTNAME + '%') OR ([EMAILID] LIKE '%' + @EMAILID + '%') OR ([PHONENO] LIKE '%' + @PHONENO + '%'))">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="TextBox1" Name="LASTNAME" PropertyName="Text" />
+                <asp:ControlParameter ControlID="TextBox1" Name="USERNAME" PropertyName="Text" />
+                <asp:ControlParameter ControlID="TextBox1" Name="FIRSTNAME" PropertyName="Text" />
+                <asp:ControlParameter ControlID="TextBox1" Name="EMAILID" PropertyName="Text" />
+                <asp:ControlParameter ControlID="TextBox1" Name="PHONENO" PropertyName="Text" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+        </div>
+    
+        <div id="div5" runat="server">
+        <asp:GridView ID="GridView2" runat="server" AllowPaging="True" AllowSorting="True"  CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateSelectButton="True" OnSelectedIndexChanged="GridView2_SelectedIndexChanged" > 
+                
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+            <br />
+            <asp:Label ID="Label5" runat="server"></asp:Label>
+        <br />
+        
+                
+        </div>
+        
+    </form>
+</body>
+</html>
+
---
+++ Thesis-project Repository/Search.aspx.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Thesis_project_Repository
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextBox1.Text))
+            {
+                div4.Visible = true;
+                div5.Visible = false;
+                DetailsView1.Visible = true;
+                DetailsView2.Visible = true;
+                TextBox2.Text = "";
+                TextBox3.Text = "";
+                Label5.Text = "";
+                RadioButtonList1.ClearSelection();
+                RadioButtonList2.ClearSelection();
+                RadioButtonList3.ClearSelection();
+                TextBox2.Visible = false;
+                TextBox3.Visible = false;
+                DropDownList2.Visible = false;
+                Label1.Text = "";
+                Label2.Text = "";
+                Label4.Text = "";
+                Label3.Text = "";
+                Label6.Text = "";
+
+            }
+            else
+            {
+                Label6.Text = "Please enter a keyword";
+                div4.Visible = false;
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "";
+            Label5.Text = "";
+            Label6.Text = "";
+            div4.Visible = false;
+            div5.Visible = true;
+            DetailsView1.Visible = false;
+            DetailsView2.Visible = false;
+            TextBox1.Text = "";
+            string criterion = DropDownList1.Text;
+            int choice;
+            string userInput;
+            string keyword = "";
+            if (DropDownList1.SelectedIndex != 0)
+            {
+                if (RadioButtonList1.SelectedItem != null || RadioButtonList2.SelectedItem != null || RadioButtonList3.SelectedItem != null)
+                {
+                    if (criterion.Equals("account type"))
+                    {
+                        choice = RadioButtonList2.SelectedIndex;
+                        if (choice != 0)
+                        {
+
+                            if (DropDownList2.SelectedIndex != 0)
+                            {
+                                userInput = DropDownList2.Text;
+                                keyword = string.Format("{0}", userInput);
+                            }
+                            else
+                            {
+                                Label2.Text = "Please select a type.";
+                                keyword = "0";
+                            }
+
+                        }
+                    }
+                    else if (criterion.Equals("username"))
+                    {
+                        choice = RadioButtonList1.SelectedIndex;
+                        userInput = TextBox3.Text.ToLower();
+                        if (!string.IsNullOrWhiteSpace(userInput))
+                        {
+                            keyword = string.Format("%{0}%", userInput);
+                        }
+                        else
+                        {
+                            Label3.Text = "Please enter a keyword into the textbox.";
+                            keyword = "0";
+                        }
+                    }
+                    else
+                    {
+                        choice = RadioButtonList1.SelectedIndex;
+                        if (choice != 0)
+                        {
+                            userInput = TextBox2.Text.ToLower();
+                            if (!string.IsNullOrWhiteSpace(userInput))
+                            {
+                                keyword = string.Format("%{0}%", userInput);
+                            }
+                            else
+                            {
+                                Label1.Text = "Please enter a keyword into the textbox.";
+                                keyword = "0";
+
+                            }
+
+                        }
+
+                    }
+                    GetResultsByCriterion(criterion, keyword);
+                }
+
+                else
+                {
+                    Label4.Text = "Please select an option";
+                }
+            }
+            else
+            {
+                Label4.Text = "Please select a search criterion";
+            }
+        }
+        public void GetResultsByCriterion(string criterion, string keyword)
+        {
+            LoginDSTableAdapters.LOGININFOTableAdapter loginInfoTableAdapter = new LoginDSTableAdapters.LOGININFOTableAdapter();
+            UserDSTableAdapters.USERINFOTableAdapter userInfoTableAdapter = new UserDSTableAdapters.USERINFOTableAdapter();
+            if (criterion.Equals("username"))
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    GridView2.DataSource = userInfoTableAdapter.GetDataByUsername(keyword);
+                }
+
+
+            }
+            else if (criterion.Equals("first name"))
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    if (!keyword.Equals("0"))
+                    {
+
+                        GridView2.DataSource = userInfoTableAdapter.GetDataByFirstName(keyword);
+                    }
+                }
+                else
+                {
+                    GridView2.DataSource = userInfoTableAdapter.GetDataByAllFirstName();
+                }
+            }
+            else if (criterion == "last name")
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    if (!keyword.Equals("0"))
+                    {
+
+                        GridView2.DataSource = userInfoTableAdapter.GetDataByLastName(keyword);
+                    }
+                }
+                else
+                {
+                    GridView2.DataSource = userInfoTableAdapter.GetDataByAllLastName();
+                }
+            }
+            else if (criterion == "email")
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    if (!keyword.Equals("0"))
+                    {
+                        GridView2.DataSource = userInfoTableAdapter.GetDataByEmail(keyword);
+                    }
+                }
+                else
+                {
+                    GridView2.DataSource = userInfoTableAdapter.GetDataByAllEmail();
+                }
+            }
+            else if (criterion == "phone number")
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    if (!keyword.Equals("0"))
+                    {
+                        GridView2.DataSource = userInfoTableAdapter.GetDataByPhone(keyword);
+                    }
+                }
+                else
+                {
+                    GridView2.DataSource = userInfoTableAdapter.GetDataByAllPhone();
+                }
+            }
+            else if (criterion == "account type")
+            {
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    if (!keyword.Equals("0"))
+                    {
+                        GridView2.DataSource = loginInfoTableAdapter.GetDataByAccountType(keyword);
+                    }
+                }
+                else
+                {
+                    GridView2.DataSource = loginInfoTableAdapter.GetDataByAllAccountType();
+                }
+            }
+            GridView2.DataBind();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedValue.Equals("account type"))
+            {
+                div1.Visible = false;
+                div2.Visible = true;
+                div3.Visible = false;
+            }
+            else if (DropDownList1.SelectedValue.Equals("username"))
+            {
+                div1.Visible = false;
+                div2.Visible = false;
+                div3.Visible = true;
+            }
+            else
+            {
+                div1.Visible = true;
+                div2.Visible = false;
+                div3.Visible = false;
+            }
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "";
+            if (RadioButtonList1.SelectedIndex == 1)
+            {
+                TextBox2.Visible = true;
+            }
+            else
+            {
+                TextBox2.Visible = false;
+            }
+        }
+        protected void RadioButtonList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "";
+            if (RadioButtonList2.SelectedIndex == 1)
+            {
+                DropDownList2.Visible = true;
+            }
+            else
+            {
+                DropDownList2.Visible = false;
+            }
+        }
+
+        protected void RadioButtonList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "";
+            if (RadioButtonList3.SelectedIndex == 0)
+            {
+                TextBox3.Visible = true;
+            }
+        }
+
+        protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string selectedLink = GridView2.SelectedRow.Cells[3].Text;
+            string result = GetDetailsForAdvancedSearch(selectedLink);
+            Label5.Text = result;
+        }
+
+        public string GetDetailsForAdvancedSearch(string userInput)
+        {
+            // string connectionString = "Data Source=itksqlexp8;Initial Catalog=AJAIN5_ConservationSchool;"
+            //                            + "Integrated Security=true";
+            string connectionString = "Data Source=itksqlexp8;Initial Catalog=it485project;"
+                                         + "Integrated Security=true";
+
+            string queryString = "SELECT * from userinfo "
+                          + "WHERE username = '" + userInput + "';";
+
+            string result = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result += "First name:" + reader[0].ToString() + "<br />";
+                        result += "Last name is :" + reader[1].ToString() + "<br />";
+                        result += "Username:" + reader[2].ToString() + "<br />";
+                        result += "Email :" + reader[3].ToString() + "<br />";
+                        result += "Phone Number :" + reader[5].ToString() + "<br />";
+
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            connectionString = "Data Source=itksqlexp8;Initial Catalog=it485project;"
+                                         + "Integrated Security=true";
+
+            queryString = "SELECT acctype from logininfo "
+                          + "WHERE username = '" + userInput + "';";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result += "Account Type:" + reader[0].ToString() + "<br />";
+
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return result;
+        }
+    }
+}
